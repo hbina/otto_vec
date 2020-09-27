85029b1059c6efe2f574bc09c2c45354de9361bd
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2020-09-27 08:41:23 +0800

    Now supports an additional argument pattern.
    Signed-off-by: Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>

--- otto_vec_testbed/src/functions.rs
+++ otto_vec_testbed/src/functions.rs
@@ -1,20 +1,34 @@
+#[allow(dead_code)]
 #[otto_vec]
-pub fn function_1(a: i32, b: i32) -> i32 {
+pub(crate) fn function_1(a: i32, b: i32) -> i32 {
     a + b
 }
 
+#[allow(dead_code)]
 #[otto_vec]
-pub fn function_2<X: Into<i32>, Y: Into<i32>>(a: X, b: Y) -> i32 {
+pub(crate) fn function_2<X: Into<i32>, Y: Into<i32>>(a: X, b: Y) -> i32 {
     let result = 10;
     result * a.into() + b.into()
 }
 
+#[allow(dead_code)]
 #[otto_vec]
-pub fn function_3(a: Option<usize>, b: Option<usize>) -> Option<usize> {
+pub(crate) fn function_3(a: Option<usize>, b: Option<usize>) -> Option<usize> {
     if let Some(a) = a {
         if let Some(b) = b {
             return Some(a + b);
         }
     }
-    return None;
+    None
+}
+
+pub struct Location {
+    x: i64,
+    y: i64,
+}
+
+#[allow(dead_code)]
+#[otto_vec]
+fn fn_4((Location { x, .. }, Location { y, .. }): (Location, Location)) -> i64 {
+    x * y
 }
--- otto_vec_testbed/src/tests.rs
+++ otto_vec_testbed/src/tests.rs
@@ -1,11 +1,12 @@
-use crate::functions::{function_1_vec, function_2_vec, function_3_vec};
-
 #[test]
 fn test_function_1() {
     let expected = vec![2, 5, 7, 12, 3];
     let vector_1 = vec![1, 3, 3, 7, 2];
     let vector_2 = vec![1, 2, 4, 5, 1];
-    assert_eq!(expected, function_1_vec(vector_1, vector_2));
+    assert_eq!(
+        expected,
+        crate::functions::function_1_vec(vector_1, vector_2)
+    );
 }
 
 #[test]
@@ -27,7 +28,10 @@ fn test_function_2() {
     let expected = vec![11, 32, 34, 75, 21];
     let vector_1 = vec![S::new(1), S::new(3), S::new(3), S::new(7), S::new(2)];
     let vector_2 = vec![S::new(1), S::new(2), S::new(4), S::new(5), S::new(1)];
-    assert_eq!(expected, function_2_vec(vector_1, vector_2));
+    assert_eq!(
+        expected,
+        crate::functions::function_2_vec(vector_1, vector_2)
+    );
 }
 
 #[test]
@@ -35,5 +39,8 @@ fn test_function_3() {
     let expected = vec![Some(4), Some(4), None, None];
     let vector_1 = vec![Some(1), Some(2), Some(3), None];
     let vector_2 = vec![Some(3), Some(2), None, Some(0)];
-    assert_eq!(expected, function_3_vec(vector_1, vector_2));
+    assert_eq!(
+        expected,
+        crate::functions::function_3_vec(vector_1, vector_2)
+    );
 }
